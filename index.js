394fb178a7739071d1a4a85d7701f51ed12ce863
Bilkis Ismail <bilkisismail07@gmail.com>
2023-10-03 22:39:06 +0700

    Update lesson

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.APP_PORT;
 const database = require("./database");
+const cors = require("cors");
+const helmet = require("helmet");
 
 // grant access for express can accept input from outside
 app.use(express.urlencoded({ extended: false }));
 // parse application/json
 app.use(express.json());
+// grant access for all client using this resource
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
+// using helmet
+app.use(helmet());
 
 // ENDPOINT MOVIES
 app.get("/movies", async (req, res) => {
@@ -98,7 +111,64 @@ app.post("/movies", async (req, res) => {
   }
 });
 
+app.put("/movies/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const columns = [
+      "name",
+      "release_date",
+      "duration",
+      "directed_by",
+      "genres",
+      "casts",
+      "synopsis",
+      "poster",
+    ];
+
+    const request = await database`
+      UPDATE movies SET ${database(
+        req.body,
+        columns
+      )} WHERE id = ${id} RETURNING id
+    `;
+
+    if (request.length > 0) {
+      res.json({
+        status: true,
+        message: "Update data success",
+      });
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(502).json({
+      status: false,
+      message: "Something wrong in our server",
+      data: [],
+    });
+  }
+});
+
+app.delete("/movies/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const request = await database`DELETE FROM movies WHERE id = ${id}`;
+
+    res.json({
+      status: true,
+      message: "Delete data success",
+      data: request,
+    });
+  } catch (error) {
+    res.status(502).json({
+      status: false,
+      message: "Something wrong in our server",
+      data: [],
+    });
+  }
+});
+
 // ENDPOINT CINEMAS
+
 // ENDPOINT USERS
 
 app.listen(port, () => {
